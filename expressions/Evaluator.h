8434c639c1539c346e5b5b60f250db13a6b55072
Mark Boorer <mkj@dneg.com>
2013-11-15 18:22:46 +0000

    Massive fixes. Works with LLVM 3.2

--- expressions/Evaluator.h
+++ expressions/Evaluator.h
@@ -13,14 +13,15 @@
 #include "llvm/DerivedTypes.h"
 #include "llvm/ExecutionEngine/ExecutionEngine.h"
 #include "llvm/ExecutionEngine/JIT.h"
+#include "llvm/IRBuilder.h"
 #include "llvm/LLVMContext.h"
 #include "llvm/Module.h"
+#include "llvm/Intrinsics.h"
 #include "llvm/PassManager.h"
 #include "llvm/Analysis/Verifier.h"
 #include "llvm/Analysis/Passes.h"
-#include "llvm/Target/TargetData.h"
+#include "llvm/DataLayout.h"
 #include "llvm/Transforms/Scalar.h"
-#include "llvm/Support/IRBuilder.h"
 #include "llvm/Support/TargetSelect.h"
 
 #endif //USE_LLVM
@@ -187,7 +188,7 @@ class LLVMEvaluator
 	public:
 		typedef std::map<std::string, T> VariableMap;
 
-		LLVMEvaluator(ASTNode *ast)
+		LLVMEvaluator(ASTNode *ast, VariableMap *map=NULL)
 			: m_context(llvm::getGlobalContext())
 			, m_module(new llvm::Module("expression jit", m_context))
 			, m_builder(m_context)
@@ -198,6 +199,8 @@ class LLVMEvaluator
 			{
 				throw EvaluatorException("Incorrect abstract syntax tree");
 			}
+
+			llvm::InitializeNativeTarget();
 
 			// Set up the JIT compiler
 			std::string error;
@@ -210,9 +213,10 @@ class LLVMEvaluator
 				throw EvaluatorException(ss.str().c_str());
 			}
 
+			
 			// Set up the optimiser pipeline
 			// Register how target lays out data structures
-			m_fpm.add(new llvm::TargetData(*m_engine->getTargetData()));
+			m_fpm.add(new llvm::DataLayout(*m_engine->getDataLayout()));
 			// Provide basic AliasAnalysis support for GVN
 			m_fpm.add(llvm::createBasicAliasAnalysisPass());
 			// Do simple "peephole" optimisations and bit-twiddling
@@ -225,24 +229,62 @@ class LLVMEvaluator
 			m_fpm.add(llvm::createCFGSimplificationPass());
 
 			m_fpm.doInitialization();
+			
+
+			// Create Function as entry point for LLVM
+			std::vector<llvm::Type*> Void(0, llvm::Type::getFloatTy(m_context));
+			llvm::FunctionType *FT = llvm::FunctionType::get(llvm::Type::getFloatTy(m_context), Void, false);
+			llvm::Function *LF = llvm::Function::Create(FT, llvm::Function::ExternalLinkage, "", m_module);
+			
+			// Create block for code
+			llvm::BasicBlock *BB = llvm::BasicBlock::Create(m_context, "entry", LF);
+			m_builder.SetInsertPoint(BB);
+
+			if (map)
+			{
+				// Take the values from VariableMap and add entries to our internal map
+				for (typename VariableMap::iterator it = map->begin(); it != map->end(); it++)
+				{
+					llvm::IRBuilder<> tmpB(&LF->getEntryBlock(), LF->getEntryBlock().begin());
+					llvm::AllocaInst *alloca = tmpB.CreateAlloca(llvm::Type::getFloatTy(m_context), 0, it->first.c_str());
+					llvm::Value *val = llvm::ConstantFP::get(m_context, llvm::APFloat(it->second));
+					m_builder.CreateStore(val, alloca);
+					m_map[it->first] = alloca;
+				}
+			}
 
 
-			// Get function from LLVM
-			std::vector<llvm::Type*> Doubles(0, Type::getDoubleTy(m_context));
-			llvm::FunctionType *FT = FunctionType::get(Type::getDoubleTy(m_context),Doubles, false);
-			llvm::Function *LF = Function::Create(FT, Function::ExternalLinkage, "go", m_module);
-generateLLVM(ast);
-	
-			LF->dump();
+			// Convert AST to LLVM and place into function pointer
+			m_builder.CreateRet(generateLLVM(ast));
 
+			//llvm::verifyFunction(*LF); /* This fails when intrinsics are used */
+			//LF->dump(); /* Dump the IR code*/
+
+			// Set the execute function call
 			void *FPtr = m_engine->getPointerToFunction(LF);
-			double (*FP)() = (double (*)()) (intptr_t)FPtr;
-
-			std::cout << "result: " << FP() << std::endl;
+			m_function = (float (*)()) (intptr_t)FPtr;
 		}
+
+		T evaluate()
+		{
+			return m_function();
+		}
+
+		//TODO fix these, Need to capture when they set a value and do the same call as the VariableMao setting
+		/*
+		T& operator[](std::string key)
+		{
+			return m_map[key];
+		}
+
+		const T& operator[](std::string key) const
+		{
+			return m_map[key];
+		}*/
 
 	private:
 
+		// Convert AST into LLVM
 		llvm::Value *generateLLVM(ASTNode* ast)
 		{
 			if(!ast)
@@ -252,26 +294,22 @@ generateLLVM(ast);
 			if(ast->type() == ASTNode::NUMBER)
 			{
 				NumberASTNode<T>* n = static_cast<NumberASTNode<T>* >(ast);
-				return llvm::ConstantFP::get(llvm::getGlobalContext(), llvm::APFloat(n->value()));
+				return llvm::ConstantFP::get(m_context, llvm::APFloat(n->value()));
 			}
-			/*else if(ast->type() == ASTNode::VARIABLE)
+			else if(ast->type() == ASTNode::VARIABLE)
 			{
-				//TODO investigate this
 				VariableASTNode<T>* v = static_cast<VariableASTNode<T>* >(ast);
 				std::string variable = v->variable();
-				if (!map)
-				{
-					throw EvaluatorException("Variable encountered but no VariableMap provided");
-				}
 
-				if (!map->count(variable))
+				if (!m_map.count(variable))
 				{
 					std::ostringstream ss;
 					ss << "No variable '" << variable << "' defined in VariableMap";
 					throw Exception(ss.str().c_str());
 				}
-				return map->at(variable);
-			}*/
+				// Load the value from the map
+				return m_builder.CreateLoad(m_map[variable], variable.c_str());
+			}
 			else if (ast->type() == ASTNode::OPERATION)
 			{
 				OperationASTNode* op = static_cast<OperationASTNode*>(ast);
@@ -284,27 +322,47 @@ generateLLVM(ast);
 					case OperationASTNode::MINUS: return m_builder.CreateFSub(v1, v2, "subtmp");
 					case OperationASTNode::MUL:   return m_builder.CreateFMul(v1, v2, "multmp");
 					case OperationASTNode::DIV:   return m_builder.CreateFDiv(v1, v2, "divtmp");
-				//	case OperationASTNode::POW:   return (T) pow(v1,v2);
+					case OperationASTNode::POW:
+					{
+						// Pow operator is defined in an intrinsic, so implement as a function call
+						std::vector<llvm::Type*> arg_types(2, llvm::Type::getFloatTy(m_context)); // args are 2 floats
+						return m_builder.CreateCall2(llvm::Intrinsic::getDeclaration(m_module, llvm::Intrinsic::pow, arg_types), v1, v2, "powtmp");
+					}
 					case OperationASTNode::MOD:   return m_builder.CreateFRem(v1, v2, "modtmp");
 					default: throw EvaluatorException("Unknown operator in syntax tree");
 				}
-			}/*
+			}
 			else if (ast->type() == ASTNode::FUNCTION1)
 			{
 				Function1ASTNode* f = static_cast<Function1ASTNode*>(ast);
 
-				T v1 = evaluateSubtree(f->left());
+				llvm::Value *v1 = generateLLVM(f->left());
+				std::vector<llvm::Type*> arg_types(1, llvm::Type::getFloatTy(m_context)); // args are 1 float
 				switch(f->function())
 				{
-					case Function1ASTNode::SIN:   return (T) sin(v1);
-					case Function1ASTNode::COS:   return (T) cos(v1);
-					case Function1ASTNode::TAN:   return (T) tan(v1);
-					case Function1ASTNode::SQRT:  return (T) sqrt(v1);
-					case Function1ASTNode::LOG:   return (T) log(v1);
-					case Function1ASTNode::LOG2:  return (T) log2(v1);
-					case Function1ASTNode::LOG10: return (T) log10(v1);
-					case Function1ASTNode::CEIL:  return (T) ceil(v1);
-					case Function1ASTNode::FLOOR: return (T) floor(v1);
+					case Function1ASTNode::SIN:   return m_builder.CreateCall(llvm::Intrinsic::getDeclaration(m_module, llvm::Intrinsic::sin, arg_types), v1, "sintmp");
+					case Function1ASTNode::COS:   return m_builder.CreateCall(llvm::Intrinsic::getDeclaration(m_module, llvm::Intrinsic::cos, arg_types), v1, "costmp");
+					//case Function1ASTNode::TAN: return m_builder.CreateCall(llvm::Intrinsic::getDeclaration(m_module, llvm::Intrinsic::tan, arg_types), v1 ,"tantmp");
+					case Function1ASTNode::TAN:
+					{
+						// No tan operator in LLVM 3.2. Have to use sin0/cos0
+						llvm::Value *sin = m_builder.CreateCall(llvm::Intrinsic::getDeclaration(m_module, llvm::Intrinsic::sin, arg_types), v1, "sintmp");
+						llvm::Value *cos = m_builder.CreateCall(llvm::Intrinsic::getDeclaration(m_module, llvm::Intrinsic::cos, arg_types), v1, "costmp");
+						return m_builder.CreateFDiv(sin, cos, "tantmp");
+					}
+					case Function1ASTNode::SQRT:  return m_builder.CreateCall(llvm::Intrinsic::getDeclaration(m_module, llvm::Intrinsic::sqrt, arg_types), v1, "sqrttmp");
+					case Function1ASTNode::LOG:   return m_builder.CreateCall(llvm::Intrinsic::getDeclaration(m_module, llvm::Intrinsic::log, arg_types), v1, "logtmp");
+					case Function1ASTNode::LOG2:  return m_builder.CreateCall(llvm::Intrinsic::getDeclaration(m_module, llvm::Intrinsic::log2, arg_types), v1, "log2tmp");
+					case Function1ASTNode::LOG10: return m_builder.CreateCall(llvm::Intrinsic::getDeclaration(m_module, llvm::Intrinsic::log10, arg_types), v1, "log10tmp");
+					//case Function1ASTNode::CEIL:  return m_builder.CreateCall(llvm::Intrinsic::getDeclaration(m_module, llvm::Intrinsic::ceil, arg_types), v1, "ceiltmp");
+					case Function1ASTNode::CEIL:
+					{
+						// No ceil operator in LLVM 3.2. Have to use floor +1
+						llvm::Value *floor = m_builder.CreateCall(llvm::Intrinsic::getDeclaration(m_module, llvm::Intrinsic::floor, arg_types), v1, "floortmp");
+						llvm::Value *one = llvm::ConstantFP::get(m_context, llvm::APFloat(1.0));
+						return m_builder.CreateFAdd(floor, one, "ceiltmp");
+					}
+					case Function1ASTNode::FLOOR: return m_builder.CreateCall(llvm::Intrinsic::getDeclaration(m_module, llvm::Intrinsic::floor, arg_types), v1, "floortmp");
 					default: throw EvaluatorException("Unknown function in syntax tree");
 				}
 			}
@@ -312,57 +370,99 @@ generateLLVM(ast);
 			{
 				Function2ASTNode* f = static_cast<Function2ASTNode*>(ast);
 
-				T v1 = evaluateSubtree(f->right()); // the operators are switched thanks to rpn notation
-				T v2 = evaluateSubtree(f->left());
+				llvm::Value *v1 = generateLLVM(f->right()); // the operators are switched thanks to rpn notation
+				llvm::Value *v2 = generateLLVM(f->left());
 				switch(f->function())
 				{
-					case Function2ASTNode::MIN:  return std::min(v1, v2);
-					case Function2ASTNode::MAX:  return std::max(v1, v2);
-					case Function2ASTNode::POW:  return (T) pow(v1, v2);
+					case Function2ASTNode::MIN:  
+					{
+						// Min is 2 operations, Ordered Greater Than, then select
+						llvm::Value *gt = m_builder.CreateFCmpOGT(v1, v2, "fogttmp");
+						return m_builder.CreateSelect(gt, v2, v1, "mintmp");
+					}
+					case Function2ASTNode::MAX:
+					{
+						// Max is 2 operations, Ordered Greater Than, then select
+						llvm::Value *gt = m_builder.CreateFCmpOGT(v1, v2, "fogttmp");
+						return m_builder.CreateSelect(gt, v1, v2, "maxtmp");
+					}
+					case Function2ASTNode::POW: 
+					{
+						// Pow operator is defined in an intrinsic, so implement as a function call
+						std::vector<llvm::Type*> arg_types(2, llvm::Type::getFloatTy(m_context)); // args are 2 floats
+						return m_builder.CreateCall2(llvm::Intrinsic::getDeclaration(m_module, llvm::Intrinsic::pow, arg_types), v1, v2, "powtmp");
+					}
 					default: throw EvaluatorException("Unknown function in syntax tree");
 				}
 			}
 			else if (ast->type() == ASTNode::COMPARISON)
 			{
 				ComparisonASTNode* c = static_cast<ComparisonASTNode*>(ast);
-				T v1 = evaluateSubtree(c->right()); // the operators are switched thanks to rpn notation
-				T v2 = evaluateSubtree(c->left());
+				llvm::Value *v1 = generateLLVM(c->right()); // the operators are switched thanks to rpn notation
+				llvm::Value *v2 = generateLLVM(c->left());
 				switch(c->comparison())
 				{
-					case ComparisonASTNode::EQUAL:              return v1 == v2;
-					case ComparisonASTNode::NOT_EQUAL:          return v1 != v2;
-					case ComparisonASTNode::GREATER_THAN:       return v1 >  v2;
-					case ComparisonASTNode::GREATER_THAN_EQUAL: return v1 >= v2;
-					case ComparisonASTNode::LESS_THAN:          return v1 <  v2;
-					case ComparisonASTNode::LESS_THAN_EQUAL:    return v1 <= v2;
+					case ComparisonASTNode::EQUAL:              return m_builder.CreateFCmpOEQ(v1, v2, "foeqtmp");
+					case ComparisonASTNode::NOT_EQUAL:          return m_builder.CreateFCmpONE(v1, v2, "fonetmp");
+					case ComparisonASTNode::GREATER_THAN:       return m_builder.CreateFCmpOGT(v1, v2, "fogttmp");
+					case ComparisonASTNode::GREATER_THAN_EQUAL: return m_builder.CreateFCmpOGE(v1, v2, "fogetmp");
+					case ComparisonASTNode::LESS_THAN:          return m_builder.CreateFCmpOLT(v1, v2, "folttmp");
+					case ComparisonASTNode::LESS_THAN_EQUAL:    return m_builder.CreateFCmpOLE(v1, v2, "foletmp");
 					default: throw EvaluatorException("Unknown comparison in syntax tree");
 				}
 			}
 			else if (ast->type() == ASTNode::LOGICAL)
 			{
 				LogicalASTNode *l = static_cast<LogicalASTNode*>(ast);
-				T v1 = evaluateSubtree(l->right()); // the operators are switched thanks to rpn notation
-				T v2 = evaluateSubtree(l->left());
+				llvm::Value *v1 = generateLLVM(l->right()); // the operators are switched thanks to rpn notation
+				llvm::Value *v2 = generateLLVM(l->left());
 				switch(l->operation())
 				{
-					case LogicalASTNode::AND: return v1 && v2;
-					case LogicalASTNode::OR:  return v1 || v2;
+					case LogicalASTNode::AND: return m_builder.CreateAnd(v1, v2, "andtmp");
+					case LogicalASTNode::OR:  return m_builder.CreateOr(v1, v2, "ortmp");
 					default: throw EvaluatorException("Unknown logical operator in syntax tree");
 				}
 			}
 			else if (ast->type() == ASTNode::BRANCH)
 			{
 				BranchASTNode* b = static_cast<BranchASTNode*>(ast);
-				if ((bool)evaluateSubtree(b->condition()) == true)
+
+				llvm::Value *cond = generateLLVM(b->condition());
+				// If condition is a number, coerce it to bool
+				if (cond->getType() == llvm::Type::getFloatTy(m_context))
 				{
-					return evaluateSubtree(b->yes());
+					cond = m_builder.CreateFCmpONE(cond, llvm::ConstantFP::get(m_context, llvm::APFloat(0.0f)), "ifcond");
 				}
-				else
-				{
-					return evaluateSubtree(b->no());
-				}
+				llvm::Value *yes  = generateLLVM(b->yes());
+				llvm::Value *no   = generateLLVM(b->no());
+
+				llvm::Function *fun = m_builder.GetInsertBlock()->getParent();
+				llvm::BasicBlock *thenBB  = llvm::BasicBlock::Create(m_context, "then", fun);
+				llvm::BasicBlock *elseBB  = llvm::BasicBlock::Create(m_context, "else");
+				llvm::BasicBlock *mergeBB = llvm::BasicBlock::Create(m_context, "ifcont");
+
+				m_builder.CreateCondBr(cond, thenBB, elseBB);
+				m_builder.SetInsertPoint(thenBB);
+	
+				m_builder.CreateBr(mergeBB);
+
+				// emit else block
+				thenBB = m_builder.GetInsertBlock();
+				fun->getBasicBlockList().push_back(elseBB);
+				m_builder.SetInsertPoint(elseBB);
+
+				m_builder.CreateBr(mergeBB);
+				elseBB = m_builder.GetInsertBlock();
+
+				// emit the merge block
+				fun->getBasicBlockList().push_back(mergeBB);
+				m_builder.SetInsertPoint(mergeBB);
+				llvm::PHINode *PN = m_builder.CreatePHI(llvm::Type::getFloatTy(m_context), 2, "iftmp");
+				PN->addIncoming(yes, thenBB);
+				PN->addIncoming(no, elseBB);
+				return PN;
 			}
-			*/
+
 			throw EvaluatorException("Incorrect syntax tree!");
 
 		}
@@ -375,7 +475,9 @@ generateLLVM(ast);
 		llvm::FunctionPassManager m_fpm;
 		std::map<std::string, llvm::AllocaInst*> m_map;
 
+		T (*m_function)();
 };
+
 #endif // USE_LLVM
 
 } // namespace expr
